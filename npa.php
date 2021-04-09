d27e912897f0b54fc9583b34c1b35e47428022c6
vagon512 <vagon512@yandex.ru>
2021-04-09 19:42:58 +0300

    edit npa.php

--- npa.php
+++ npa.php
@@ -10,26 +10,7 @@ showHead($pageName, $pageCon);
 
 showTree('myfiles', " ");
 
-//pdf parse
-  $parser = new \Smalot\PdfParser\Parser();
 
-  $pdf = $parser->parseFile($filename);
-  $text = $pdf->getText();
-  echo strlen($text);
-  $text_result=explode(" ", $text);
-//  echo "<br>".sizeof($text_result);
-//  echo "<br>".$text_result[0]." ".$text_result[50];
-$j=0;
-echo "<p>";
-for($i=0; $i<=sizeof($text_result);$i++){
-  if($j==60){
-    echo "<br>";
-    $j=0;
-  }
-  $j++;
-  echo $text_result[$i]." ";
-}
-  echo "</p>";
 showFoot();
 ?>
 
