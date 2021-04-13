d5f3adf2be996eaa4b888fbcb959bbd45a5669ca
vagon512 <vagon512@yandex.ru>
2021-04-13 11:10:52 +0300

    add style for ul

--- npa.php
+++ npa.php
@@ -9,21 +9,23 @@ $dirName = "/mnt/npa";
 showHead($pageName, $pageCon);
 //$dir = "/mnt/npa";
 if (!$_GET["dir"]){
- echo "<div class=\"help\">hasdfsadfsadfsadfdsafsadfello</div>";
+// echo "<div class=\"my_p\">hasdfsadfsadfsadfdsafsadfello</div>";
   $myDir = scandir($dirName);
+  echo "<ul class=\"my_ul\">";
   foreach($myDir as $Dir){
     if ($Dir == "." || $Dir == ".." || $Dir == "lost+found") continue;
    $r = $dirName."/".$Dir;
     if(is_dir($r)){
-      echo "<a href = npa.php?dir=$Dir>".$Dir."</a><br>";
+      echo "<li><a href = npa.php?dir=$Dir>".$Dir."</a></li>";
     }
   }
+  echo "</ul>";
 }
 
 else{
   $dir = $dirName."/".$_GET["dir"];
  // echo "<style> div{color:red; border-color: green;}</style>";
-  echo "<div><a href = \"npa.php\">go back</a></div>";
+  echo "<p class=my_p><a href = \"npa.php\">go back</a></p>";
   showTree($dir, "==");
 }
 
--- result.php
+++ result.php
@@ -19,13 +19,10 @@ echo $filename;
   $pdf = $parser->parseFile($filename);
   $text = $pdf->getText();
   $num = strpos($text, "А");
-//echo "<p>".$num."</p>";
   $text = substr($text, $num);
-  //echo strlen($text);
   $text_result=explode(" ", $text);
   $new_text=array();
-//  echo "<br>".sizeof($text_result);
-//  echo "<br>".$text_result[0]." ".$text_result[50];
+
 for($i=0; $i<=sizeof($text_result); $i++){
   if(strlen($text_result[$i]) != 0 and $text_result[$i] != "\n" and $text_result[$i] != "\t" 
             and $text_result[$i] != "\r" and $text_result[$i] != " "){
@@ -33,9 +30,8 @@ for($i=0; $i<=sizeof($text_result); $i++){
   }
 }
 
-echo "=== ".sizeof($new_text)." ===";
 $j=0;
-echo "<p>";
+echo "<p class=my_p>";
 for($i=0; $i<=sizeof($new_text);$i++){
   if($j==30){
     echo "<br>";
@@ -51,22 +47,9 @@ for($i=0; $i<=sizeof($new_text);$i++){
 }
   echo "</p>";
 
-
-for($i=0; $i<sizeof($new_text); $i++){
-  echo $i." ===> ".$new_text[$i]."<br>";
-}
 //echo bin2hex(" ");
 
-$textW = str_split($new_text[80]);
-echo "<br>++++".sizeof($textW);
-for($i=0; $i<sizeof($textW); $i++){
-  echo $textW[$i].">>>>".bin2hex($textW[$i])."<br>";
-}
-echo bin2hex($new_text[142])."####<br>";
 
-echo strpos($new_text[80], "\t")."!!!!<br>";
-$new_text[135] =  str_replace("\t", "", $new_text[135]);
-echo ".... ".$new_text[135]."....";
 showFoot();
 ?>
 
